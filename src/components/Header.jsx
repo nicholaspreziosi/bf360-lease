6af6b696f16b93e854716815f2acae2711e2fc65
nicholaspreziosi <108760764+nicholaspreziosi@users.noreply.github.com>
2024-03-20 15:37:42 -0400

    Edit css files to make app responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,14 @@ import "../App.css";
 function Header() {
   return (
     <>
-      <div className="header">
+      <div id="header">
         <img
           className="logo"
           id="header-logo"
           src="../public/bf360logo.png"
           alt=""
         />
-        <h1>Lease Disclaimer Form</h1>
+        <h1 id="header-text">Lease Disclaimer Form</h1>
       </div>
     </>
   );
